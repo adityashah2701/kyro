e7ac4d3de214775f18a2f98a2ef02c6a92c72b14
adityashah2701 <adityashah2701@gmail.com>
2026-07-01 17:34:24 +0530

    chore: remove unused database query test script

--- query.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require("pg");
-const c = new Client("postgresql://kyro:kyro_password@localhost:5432/kyro");
-c.connect()
-  .then(() =>
-    c.query('SELECT * FROM project ORDER BY \"createdAt\" DESC LIMIT 1'),
-  )
-  .then((r) => console.log(r.rows))
-  .then(() => c.end());
